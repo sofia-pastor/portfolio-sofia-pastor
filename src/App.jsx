5c7c783f056caaf2507be46d90567132dec15c46
sofia-pastor <sofiapastor93@gmail.com>
2025-04-07 11:28:25 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+
+import "./styles/App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </div>
+  );
+}
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,7 @@
+export default function Projects() {
+  return (
+    <div className="Projects">
+      <h1>Welcome to Projects</h1>
+    </div>
+  );
+}
